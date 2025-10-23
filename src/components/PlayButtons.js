82e7e1316fdb2850ab6162960a42033d645cc638
scojy012 <163612578+scojy012@users.noreply.github.com>
2025-10-23 20:04:21 +1030

    Adding titles for hovering over buttons

--- src/components/PlayButtons.js
+++ src/components/PlayButtons.js
@@ -6,11 +6,14 @@ function PlayButtons() {
         <div className="music-controls">
             <h4>👽 Music Controls 🛸</h4> {/* Using the emojis for nice appeal UI */}
             <div className="btn-group-custom" role="group" aria-label="Play and Stop Buttons">
-                <button id="play" className="music-btn play-btn">
+                <button id="play" className="music-btn play-btn" title="Play">
                     ▶ {/* Play Button using the emoji symbol */}
                 </button>
-                <button id="stop" className="music-btn stop-btn">
+                <button id="stop" className="music-btn stop-btn" title="Stop">
                     ⏹ {/* Stop Button using the emoji symbol */}
+                </button>
+                <button id="process_play" className="music-btn process-btn" title="Process & Play">
+                    🔄▶ {/* Process & Play Button using the emoji symbol */}
                 </button>
             </div>
         </div>
