87f6a709d121d090e64e5950659994e8837e3b92
scojy012 <163612578+scojy012@users.noreply.github.com>
2025-10-23 21:27:47 +1030

    Trying increase the number of rows for the text to display

--- src/components/PreProcessText.js
+++ src/components/PreProcessText.js
@@ -6,7 +6,7 @@ function PreProcessText(){
         <>
         <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
-                        <textarea className="form-control bg-dark text-white" rows="15" id="proc" ></textarea>
+                        <textarea className="form-control bg-dark text-white" rows="40" id="proc" ></textarea>
                     </div>
         </>
     );
