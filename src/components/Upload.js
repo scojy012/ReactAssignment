ede98e38bbea378183dc3f541133f90661e804b8
scojy012 <163612578+scojy012@users.noreply.github.com>
2025-10-26 12:48:57 +1030

    received an error for my upload funtion. ERROR Cannot read properties of null (reading 'files') TypeError: Cannot read properties of null (reading 'files'). I'm going to place it in a try catch error so that it can check if a file is acutally selected

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -3,25 +3,30 @@ import '../App.css';
 import PreProcessText from './PreProcessText';
 
 // upload file function with file reader
-function uploadFile(){
+function Upload(){
     
     var file = document.getElementById("load").files[0]; // we are only choosing one file [0]
 
     var reader = new FileReader(); // new file reader instance
 
     reader.onloadend = function(){ // when file reading is finished
-
-        var load = JSON.parse(reader.result); // parse the JSON string so it becomes normal text
-        alert(load);
-        document.getElementById("proc").value = load; // set the text area value to the loaded text
+        try {
+            var load = JSON.parse(reader.result); // parse the JSON string so it becomes normal text
+            alert(load);
+            document.getElementById("proc").value = load; // set the text area value to the loaded text
+        } catch (error) {
+            alert('Error parsing JSON file: ' + error.message);
+        }
     }
     reader.readAsText(file); // read the file as text
-
+    <>
     // input element for loading file
     return (
-        <input type="file" id="load" name="load" onChange={uploadFile} />
+        <input type="file" id="load" name="load" onChange={Upload} />
 
     );
 
+    </>
+
 }
-export default uploadFile;
+export default Upload;
